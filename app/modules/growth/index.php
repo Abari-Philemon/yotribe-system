424b59eeb966ef037d595c6dd48159f2c73e31ff
Abari Philemon <92051532+Abari-Philemon@users.noreply.github.com>
2026-04-23 10:00:17 +0100

    update

--- app/modules/growth/index.php
+++ app/modules/growth/index.php
@@ -85,6 +85,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 ?>
 
 <form method="POST" class="card p-3">
+<div class="row">
+
+<div class="col-md-4 mb-2">
 
 <select name="stock_id" class="form-select mb-2">
 <?php foreach($stocks as $s): ?>
@@ -93,11 +96,14 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 </option>
 <?php endforeach; ?>
 </select>
+</div>
 
 <input type="number" name="sample_count" placeholder="Sample count" class="form-control mb-2">
 <input type="number" name="total_weight_g" placeholder="Total weight (g)" class="form-control mb-2">
 
+<div class="col-12">
 <button class="btn btn-primary">Record Growth</button>
+</div>
 
 </form>
 
