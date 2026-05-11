cd84b626dc12ce78d6c317f839de959ff426f5d9
Abari Philemon <92051532+Abari-Philemon@users.noreply.github.com>
2026-05-11 19:56:05 +0100

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../helpers/permission.php';
-require_once __DIR__ . 'footer.php';
+require_once __DIR__ . '/footer.php';
     /**
      * FARM CONTEXT (SECURE)
      */
