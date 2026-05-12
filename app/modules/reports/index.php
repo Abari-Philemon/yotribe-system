43fe7deb5e59338bba8de70d8a12f3b890eb7785
Abari Philemon <92051532+Abari-Philemon@users.noreply.github.com>
2026-05-12 22:10:46 +0100

    update

--- app/modules/reports/index.php
+++ app/modules/reports/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../../middleware/auth_guard.php';
-require_once __DIR__ . '/../../middleware/authorize.php';
+
 require_once __DIR__ . '/../../middleware/farm_context.php';
 require_once __DIR__ . '/../../helpers/permission.php';
 
