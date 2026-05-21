929ce8b554a00801803fb4c6609fea3b0b001bd4
Abari Philemon <92051532+Abari-Philemon@users.noreply.github.com>
2026-05-21 07:15:33 +0100

    update

--- app/modules/staff/manage.php
+++ app/modules/staff/manage.php
@@ -228,7 +228,7 @@ require_once __DIR__ . '/../../includes/sidebar.php';
                         </td>
 
                         <td>
-                            <?= htmlspecialchars($s['farm_name'] ?? 'N/A') ?> 
+                            <?= htmlspecialchars($s['farm_id'] ?? 'N/A') ?> 
                         </td>
 
                         <td>
