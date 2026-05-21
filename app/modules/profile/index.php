2d4cd8894e7ceffd4375bb662ad610034b7f97b2
Abari Philemon <92051532+Abari-Philemon@users.noreply.github.com>
2026-05-21 08:52:31 +0100

    update

--- app/modules/profile/index.php
+++ app/modules/profile/index.php
@@ -225,7 +225,7 @@ require_once __DIR__ . '/../../includes/sidebar.php';
             <div class="card-body text-center">
 
                 <img id="avatarPreview" src="<?= $profileImage ?>" class="avatar mb-3" alt="Profile Image">
-                <img src="" alt="">
+                
 
                 <h5><?= htmlspecialchars($user['full_name']) ?></h5>
                 <p class="text-muted"><?= ucfirst($user['role']) ?></p>
