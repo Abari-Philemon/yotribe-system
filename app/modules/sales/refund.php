bd0fa04436ea67886d205731dc4bf802ec9b436c
Abari Philemon <92051532+Abari-Philemon@users.noreply.github.com>
2026-07-31 22:29:25 +0100

    Sync latest changes

--- app/modules/sales/refund.php
+++ app/modules/sales/refund.php
@@ -19,7 +19,8 @@ require_once __DIR__ . '/../../config/database.php';
 require_once __DIR__ . '/../../helpers/permission.php';
 require_once __DIR__ . '/../../helpers/csrf_helper.php';
 
-
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
 require_permission('sales.refund');
 
@@ -27,9 +28,6 @@ require_permission('sales.refund');
 $farm_id = farm_id();
 
 $page_title = 'Refund Sale';
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 $saleId = (int)($_GET['id'] ?? 0);
 
@@ -362,6 +360,7 @@ require_once __DIR__ . '/../../includes/sidebar.php';
 <!-- ==========================================================
 REFUND AUTHORIZATION
 ========================================================== -->
+<?php echo "<h1 style='color:red'>FORM STARTS HERE</h1>"; ?>
 
 <form action="refund_save.php" method="POST" id="refundForm">
 
